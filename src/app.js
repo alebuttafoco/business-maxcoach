a4ed9aac74f25f4856e5f27edf70a885cfd8f242
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-06-15 16:34:06 +0200

    complete approach section

--- src/app.js
+++ src/app.js
@@ -23,32 +23,32 @@ const app = new Vue({
         ],
         approachData:[
             {
-                img:null,
+                img: '1',
                 title:'One to One',
                 text: 'Getting the necessaty clarity about the current state to help you to improve your game.',
             },
             {
-                img: null,
+                img: '2',
                 title: 'Anywhere',
                 text: 'Access to valuable and portable program which allow you to setup and live anywhere you want.',
             },
             {
-                img: null,
+                img: '3',
                 title: 'On Time',
                 text: 'Puncuality is our top priority because it\'s an essential criteria to assess a program quality.',
             },
             {
-                img: null,
+                img: '4',
                 title: 'Online Coures',
                 text: 'Online business coaching now offers you a very powerful way to empower your business into success.',
             },
             {
-                img: null,
+                img: '5',
                 title: 'Consulting',
                 text: 'You will get a clear senso of direction for your business, throught assessment and faster results.',
             },
             {
-                img: null,
+                img: '6',
                 title: 'Self Development',
                 text: 'Business Coaching often keep you focus and develop you both in a professional and personal way.',
             },
