8204c94a53fec0b76ec6a21a4d64ccfca58e47e2
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-06-15 15:52:40 +0200

    add text features

--- src/app.js
+++ src/app.js
@@ -21,6 +21,38 @@ const app = new Vue({
                 flag: 'fr',
             }
         ],
+        approachData:[
+            {
+                img:null,
+                title:'One to One',
+                text: 'Getting the necessaty clarity about the current state to help you to improve your game.',
+            },
+            {
+                img: null,
+                title: 'Anywhere',
+                text: 'Access to valuable and portable program which allow you to setup and live anywhere you want.',
+            },
+            {
+                img: null,
+                title: 'On Time',
+                text: 'Puncuality is our top priority because it\'s an essential criteria to assess a program quality.',
+            },
+            {
+                img: null,
+                title: 'Online Coures',
+                text: 'Online business coaching now offers you a very powerful way to empower your business into success.',
+            },
+            {
+                img: null,
+                title: 'Consulting',
+                text: 'You will get a clear senso of direction for your business, throught assessment and faster results.',
+            },
+            {
+                img: null,
+                title: 'Self Development',
+                text: 'Business Coaching often keep you focus and develop you both in a professional and personal way.',
+            },
+        ]
     },
     methods:{},
     mounted(){}
